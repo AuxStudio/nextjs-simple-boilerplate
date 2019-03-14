ca73edeca7964f95a0c4c9714b46b7df780d658d
shaunsaker <shaun@aux.co.za>
2019-03-14 11:21:38 +0200

    Add global styles

--- src/app/static/styles/global/index.js
+++ src/app/static/styles/global/index.js
@@ -1,6 +1,20 @@
-import css from 'styled-jsx/css'; // eslint-disable-line
+import css from 'styled-jsx/css';
 
 const styles = css.global`
+  html, body, #__next {
+    height: 100%;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+  }
+
+  img {
+    width: 100%;
+    max-width: 100%;
+    height: auto;
+  }
 `;
 
 export default styles;
---
+++ src/app/static/styles/index.js
@@ -0,0 +1,9 @@
+import global from './global';
+import helpers from './helpers';
+
+export { global, helpers };
+
+export default {
+  global,
+  helpers,
+};
