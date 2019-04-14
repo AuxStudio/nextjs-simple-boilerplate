b51b7ca127d828209480d5db6bb9302b9342f37d
shaunsaker <shaun@aux.co.za>
2019-04-14 18:08:26 +0200

    Add PrimaryButton component

---
+++ src/app/components/PrimaryButton/styles.js
@@ -0,0 +1,15 @@
+import { rhythm } from '../../static/styles/styleConstants';
+
+const styles = {
+  container: {
+    borderRadius: 0,
+    textTransform: 'initial',
+    fontWeight: '700',
+  },
+
+  spacer: {
+    width: rhythm.hz / 2,
+  },
+};
+
+export default styles;
