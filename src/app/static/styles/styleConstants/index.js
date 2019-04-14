07a24cdaba0bc410c5571c0cea9c52df4e07c0cd
shaunsaker <shaun@aux.co.za>
2019-04-14 10:49:20 +0200

    Update styleConstants

--- src/app/static/styles/styleConstants/index.js
+++ src/app/static/styles/styleConstants/index.js
@@ -1,25 +1,11 @@
-const colors = {
-  primary: '#89BBFE',
-  accent1: '#ACEDFF',
-  accent2: '#6F8AB7',
-  transBlack: 'rgba(0, 0, 0, 0.67)',
-  transWhite: 'rgba(255, 255, 255, 0.67)',
-  grey: '#615D6C',
-  lightGrey: '#CAE5FF',
-  green: '#4caf50',
-  red: '#f44336',
-};
+import colors from './colors';
+import rhythm from './rhythm';
+import sizes from './sizes';
 
-const rhythm = {
-  vt: 16,
-  hz: 24,
-};
+export { colors, rhythm, sizes };
 
-const styleConstants = {
+export default {
   colors,
   rhythm,
+  sizes,
 };
-
-export { colors, rhythm };
-
-export default styleConstants;
