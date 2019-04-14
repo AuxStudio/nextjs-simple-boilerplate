53e5c69e6c1aedfd960109d53800b6928bd1218c
shaunsaker <shaun@aux.co.za>
2019-04-14 10:33:53 +0200

    Update routes config

--- src/app/config/routes/index.js
+++ src/app/config/routes/index.js
@@ -1,10 +1,17 @@
-const routes = [
-  {
-    id: 'home',
-    title: 'Home',
-    description: 'A bloody good show ol chap!',
+import SEO from '../SEO';
+
+const getTitle = (page) => {
+  const title = `${page} | APP NAME`; // TODO:
+
+  return title;
+};
+
+const routes = {
+  home: {
+    title: getTitle('Home'),
+    description: SEO.description,
     href: '/',
   },
-];
+};
 
 export default routes;
