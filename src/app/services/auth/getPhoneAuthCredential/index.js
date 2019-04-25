f3599144a51bac966128fbdb5873282e1b4dcb08
shaunsaker <shaun@aux.co.za>
2019-04-25 09:43:27 +0200

    Update services/getPhoneAuthCredential

--- src/app/services/auth/getPhoneAuthCredential/index.js
+++ src/app/services/auth/getPhoneAuthCredential/index.js
@@ -1,11 +1,13 @@
 import firebase from '../../firebase';
 
 export default function getPhoneAuthCredential({ confirmationResult, code }) {
+  const codeString = code.toString();
+
   return new Promise(async (resolve) => {
     const fb = await firebase();
     const credential = fb.auth.PhoneAuthProvider.credential(
       confirmationResult.verificationId,
-      code,
+      codeString,
     );
 
     resolve({ credential });
