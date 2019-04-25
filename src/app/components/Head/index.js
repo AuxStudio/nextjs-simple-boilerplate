ec99408ab8ac6d2c8f568e096f5eb49525d813d5
shaunsaker <shaun@aux.co.za>
2019-04-25 09:53:35 +0200

    Move manifest.json to static folder

--- src/app/components/Head/index.js
+++ src/app/components/Head/index.js
@@ -67,7 +67,12 @@ const HeadComponent = ({ title, description, router }) => {
       <meta key="theme-color" name="theme-color" content={primaryColor} />
 
       {/* Manifest file */}
-      <link key="manifest" type="application/manifest+json" rel="manifest" href="/manifest.json" />
+      <link
+        key="manifest"
+        type="application/manifest+json"
+        rel="manifest"
+        href="/static/manifest.json"
+      />
 
       {/* Open graph */}
       <meta key="og:url" property="og:url" content={SEO.openGraph.url} />
