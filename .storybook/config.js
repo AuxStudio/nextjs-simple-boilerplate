bf692ef7c2dcbd9993ab8cd67a5504337e1800ed
Shaun Saker <shaun@aux.co.za>
2019-07-03 16:52:37 +0200

    Remove unused storybook addons

--- .storybook/config.js
+++ .storybook/config.js
@@ -2,6 +2,7 @@ import { configure, addDecorator } from '@storybook/react';
 import { muiTheme } from 'storybook-addon-material-ui';
 
 import { theme } from '../src/app/static/styles';
+import withStore from './withStore';
 
 // automatically import all files ending in *.stories.js
 const components = require.context('../src/app', true, /.stories.js$/);
