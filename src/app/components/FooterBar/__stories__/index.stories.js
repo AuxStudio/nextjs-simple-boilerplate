766132de992f10916db3c4360272e7341b5a0b3d
Shaun Saker <shaun@aux.co.za>
2019-07-03 16:56:55 +0200

    Add FooterBar stories

---
+++ src/app/components/FooterBar/__stories__/index.stories.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+
+import FooterBar from '..';
+
+storiesOf('Components|FooterBar', module).add('default', () => <FooterBar />);
