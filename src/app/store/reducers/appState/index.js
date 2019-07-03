43ee53165349b90a86508ac6fffcf9a3600688a7
Shaun Saker <shaun@aux.co.za>
2019-07-03 18:53:57 +0200

    Update appState reducer

--- src/app/store/reducers/appState/index.js
+++ src/app/store/reducers/appState/index.js
@@ -8,7 +8,7 @@ export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case 'SET_SYSTEM_MESSAGE':
       newState = cloneObject(state);
-      newState.systemMessage = action.payload.systemMessage;
+      newState.systemMessage = action.payload;
       return newState;
 
     case 'RESET_SYSTEM_MESSAGE':
@@ -26,6 +26,31 @@ export default function reducer(state = initialState, action = {}) {
       newState.pendingTransactions = newState.pendingTransactions.filter((event) => {
         return event.id !== action.payload.eventID;
       });
+      return newState;
+
+    case 'RESET_PENDING_TRANSACTIONS':
+      newState = cloneObject(state);
+      newState.pendingTransactions = initialState.pendingTransactions;
+      return newState;
+
+    case 'ADD_SYNCED_TRANSACTION':
+      newState = cloneObject(state);
+      newState.syncedTransactions.push(action.payload.event);
+      return newState;
+
+    case 'SET_IS_LOADING':
+      newState = cloneObject(state);
+      newState.isLoading = action.payload.isLoading;
+      return newState;
+
+    case 'SET_IS_SAVING':
+      newState = cloneObject(state);
+      newState.isSaving = action.payload.isSaving;
+      return newState;
+
+    case 'SET_IS_SYNCING':
+      newState = cloneObject(state);
+      newState.isSyncing = action.payload.isSyncing;
       return newState;
 
     default:
--- src/app/store/reducers/appState/initialState.js
+++ src/app/store/reducers/appState/initialState.js
@@ -1,9 +1,13 @@
 const initialState = {
   pendingTransactions: [],
+  syncedTransactions: [],
   systemMessage: {
     message: null,
     variant: null,
   },
+  isLoading: null,
+  isSaving: null,
+  isSyncing: null,
 };
 
 export default initialState;
