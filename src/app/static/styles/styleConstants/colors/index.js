8de41218f23d3d5ab502af28e5c2cfe4f59d70de
Shaun Saker <shaun@aux.co.za>
2019-07-03 16:55:54 +0200

    Update app colors

--- src/app/static/styles/styleConstants/colors/index.js
+++ src/app/static/styles/styleConstants/colors/index.js
@@ -1,10 +1,14 @@
+const secondaryColor = 'rgba(38, 50, 56, 1.0)';
+
 const colors = {
-  primary: '#5E81AC',
-  secondary: '#81A1C1',
-  accent: '#88C0D0',
-  primaryText: '#000000',
-  secondaryText: '#615D6C',
-  lightGrey: '#CAE5FF',
+  primary: '#FF5722',
+  secondary: secondaryColor,
+  lightSecondary: 'rgba(38, 50, 56, 0.5)',
+  accent: '#00A8DD',
+  primaryText: secondaryColor,
+  secondaryText: 'rgba(38, 50, 56, 0.67)',
+  lightGrey: 'rgba(38, 50, 56, 0.05)',
+  grey: 'rgba(38, 50, 56, 0.5)',
   transBlack: 'rgba(0, 0, 0, 0.67)',
   transWhite: 'rgba(255, 255, 255, 0.67)',
   green: '#4caf50',
